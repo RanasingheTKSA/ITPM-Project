e67e81dbd9f323202f7806da9a88cc35cddaa35a
Ranasinghe TKSA <sachin.ranasinghe1998.13@gmail.com>
2022-03-05 16:30:22 +0530

    Initial Commit

---
+++ ITPM-Project/src/main/java/springboot/ItpmProjectApplication.java
@@ -0,0 +1,13 @@
+package springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItpmProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItpmProjectApplication.class, args);
+	}
+
+}
